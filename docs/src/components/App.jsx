86cfb3122fce25f911f747d24b9add0d46c3ad1f
Mira Kaminska <rachevamiroslava@gmail.com>
2018-09-01 19:52:33 +0200

    adding new days

--- docs/src/components/App.jsx
+++ docs/src/components/App.jsx
@@ -20,6 +20,16 @@ class App extends Component {
     nextweekWeather: '',
     nextweekWindSpeed: '',
     nextweekDate: '',
+    nextweekTemperatureDay2: '',
+    nextweekWindDirection2: '',
+    nextweekWeather2: '',
+    nextweekWindSpeed2: '',
+    nextweekDate2: '',
+    nextweekTemperatureDay3: '',
+    nextweekWindDirection3: '',
+    nextweekWeather3: '',
+    nextweekWindSpeed3: '',
+    nextweekDate3: '',
   };
 
   cities = [];
@@ -28,6 +38,8 @@ class App extends Component {
     this.loadCities();
     this.getCurrentWeather(3081368);
     this.getNextWeekWeather(3081368);
+    this.getNextWeekWeather2(3081368);
+    this.getNextWeekWeather3(3081368);
   }
 
   loadCities = () => {
@@ -46,7 +58,8 @@ class App extends Component {
   };
 
   getCurrentWeather = (id) => {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?id=${id}&APPID=5ef08df67684d77f946df578d29b8c5e&units=metric`)
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?id=${id}&APPID=5ef08df67684d77f946df578d29b8c5e&units=metric`,)
       .then((data) => {
         this.setState({
           currentTemperature: data.data.main.temp.toFixed(1),
@@ -60,17 +73,46 @@ class App extends Component {
   };
   getNextWeekWeather = (id) => {
     axios
-      .get(`https://api.openweathermap.org/data/2.5/forecast?id=${id}&APPID=5ef08df67684d77f946df578d29b8c5e&units=metric`)
+      .get(`https://api.openweathermap.org/data/2.5/forecast?id=${id}&APPID=5ef08df67684d77f946df578d29b8c5e&units=metric`,)
       .then((data) => {
         this.setState({
-          nextweekTemperatureDay: data.data.list[15].main.temp.toFixed(1),
-          nextweekWeather: data.data.list[15].weather[0].description,
-          currentCity: data.data.city.name,
-          nextweekWindDirection: data.data.list[15].wind.deg,
-          nextweekWindSpeed: data.data.list[15].wind.speed,
-          nextweekDate: data.data.list[15].dt_txt,
+          nextweekTemperatureDay: data.data.list[7].main.temp.toFixed(1),
+          nextweekWeather: data.data.list[7].weather[0].description,
+          nextweekWindDirection: data.data.list[7].wind.deg,
+          nextweekWindSpeed: data.data.list[7].wind.speed,
+          nextweekDate: data.data.list[7].dt_txt,
         });
         console.log(this.state.nextweekWeather);
+      });
+  };
+
+  getNextWeekWeather2 = (id) => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/forecast?id=${id}&APPID=5ef08df67684d77f946df578d29b8c5e&units=metric`,)
+      .then((data) => {
+        this.setState({
+          nextweekTemperatureDay2: data.data.list[15].main.temp.toFixed(1),
+          nextweekWeather2: data.data.list[15].weather[0].description,
+          nextweekWindDirection2: data.data.list[15].wind.deg,
+          nextweekWindSpeed2: data.data.list[15].wind.speed,
+          nextweekDate2: data.data.list[15].dt_txt,
+        });
+        console.log(this.state.nextweekWeather2);
+      });
+  };
+
+  getNextWeekWeather3 = (id) => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/forecast?id=${id}&APPID=5ef08df67684d77f946df578d29b8c5e&units=metric`, )
+      .then((data) => {
+        this.setState({
+          nextweekTemperatureDay3: data.data.list[23].main.temp.toFixed(1),
+          nextweekWeather3: data.data.list[23].weather[0].description,
+          nextweekWindDirection3: data.data.list[23].wind.deg,
+          nextweekWindSpeed3: data.data.list[23].wind.speed,
+          nextweekDate3: data.data.list[23].dt_txt,
+        });
+        console.log(this.state.nextweekWeather3);
       });
   };
 
@@ -87,6 +129,8 @@ class App extends Component {
                   cityList={this.state.cityList}
                   getCurrentWeather={this.getCurrentWeather}
                   getNextWeekWeather={this.getNextWeekWeather}
+                  getNextWeekWeather2={this.getNextWeekWeather2}
+                  getNextWeekWeather3={this.getNextWeekWeather3}
                 />
               </div>
             </div>
@@ -113,22 +157,22 @@ class App extends Component {
         </div>
         <div>
           <NextWeekWeather
-            nextweekWeather={this.state.nextweekWeather}
-            nextweekTemperatureDay={this.state.nextweekTemperatureDay}
-            currentCity={this.state.currentCity}
-            nextweekDate={this.state.nextweekDate}
-            nextweekWindDirection={this.state.nextweekWindDirection}
-            nextweekWindSpeed={this.state.nextweekWindSpeed}
+            nextweekWeather={this.state.nextweekWeather2}
+            nextweekTemperatureDay={this.state.nextweekTemperatureDay2}
+            currentCity={this.state.currentCity2}
+            nextweekDate={this.state.nextweekDate2}
+            nextweekWindDirection={this.state.nextweekWindDirection2}
+            nextweekWindSpeed={this.state.nextweekWindSpeed2}
           />
         </div>
         <div>
           <NextWeekWeather
-            nextweekWeather={this.state.nextweekWeather}
-            nextweekTemperatureDay={this.state.nextweekTemperatureDay}
-            currentCity={this.state.currentCity}
-            nextweekDate={this.state.nextweekDate}
-            nextweekWindDirection={this.state.nextweekWindDirection}
-            nextweekWindSpeed={this.state.nextweekWindSpeed}
+            nextweekWeather={this.state.nextweekWeather3}
+            nextweekTemperatureDay={this.state.nextweekTemperatureDay3}
+            currentCity={this.state.currentCity3}
+            nextweekDate={this.state.nextweekDate3}
+            nextweekWindDirection={this.state.nextweekWindDirection3}
+            nextweekWindSpeed={this.state.nextweekWindSpeed3}
           />
         </div>
       </div>
