8c5d7778badf3ec1998ab22212320217e6e8ca6a
Unknown <anita.piechocka@interia.pl>
2018-09-09 21:22:16 +0200

    update

--- docs/src/js/app.js
+++ docs/src/js/app.js
@@ -1,7 +1,7 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('../../sw.js')
+    .register('./sw.js')
     .then(function () {
       console.log('Service worker registered!');
     });
